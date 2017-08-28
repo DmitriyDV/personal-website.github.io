f3bfb5d5c12dc8ff1aa4b5e64318f3c234671195
DmitriyDV <31035451+DmitriyDV@users.noreply.github.com>
2017-08-28 19:55:38 +0300

    Create temp.js

---
+++ js/temp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewBoxInside extends React.Component {
+  render(){
+    return <div>Watch, Doctor<div>;
+  }
+}
+
+ReactDOM.render(<NewBoxInside />, document.getElementById('jocke'));
