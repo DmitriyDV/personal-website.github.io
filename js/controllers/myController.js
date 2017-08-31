6f2b4e676d2645bc9aed4db0e3eb92b1fa1e4785
DmitriyDV <31035451+DmitriyDV@users.noreply.github.com>
2017-08-31 21:45:02 +0300

    Update myController.js

--- js/controllers/myController.js
+++ js/controllers/myController.js
@@ -1,3 +1,9 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Top Some Books!'; 
+  $scope.promo = 'Just!';
+  $scope.product = {
+    name: 'The First some Book.', 
+    price: 19,
+    pubdate: new Date('2017','01','24')
+  }
 }]);
