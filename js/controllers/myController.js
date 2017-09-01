e5938aa1413ce36e080b004d003ce18e6e3348da
DmitriyDV <31035451+DmitriyDV@users.noreply.github.com>
2017-09-01 15:51:06 +0300

    Update myController.js

--- js/controllers/myController.js
+++ js/controllers/myController.js
@@ -6,13 +6,18 @@ app.controller('MainController', ['$scope', function($scope) {
     name: 'The Book of Trees', 
     price: 19, 
     pubdate: new Date('2014', '03', '08'), 
-    cover: 'img/planet.png' 
+    cover: 'img/planet.png',
+    likes = 0
   }, 
   { 
     name: 'Program or be Programmed', 
     price: 8, 
     pubdate: new Date('2013', '08', '01'), 
-    cover: 'img/space.png' 
+    cover: 'img/space.png',
+    likes = 0
   } 
-]
+];
+  $scope.plusOne = function(index) {
+    $scope.products[index].likes+=1;
+  }
 }]);
