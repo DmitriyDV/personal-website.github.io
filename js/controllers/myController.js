67ddb362e16fdb20548b30436c95c494e78fcc91
DmitriyDV <31035451+DmitriyDV@users.noreply.github.com>
2017-09-19 14:58:15 +0300

    Update myController.js
    sinta... error

--- js/controllers/myController.js
+++ js/controllers/myController.js
@@ -7,14 +7,14 @@ app.controller('MainController', ['$scope', function($scope) {
     price: 19, 
     pubdate: new Date('2014', '03', '08'), 
     cover: 'img/planet.png',
-    likes = 0
+    likes : 0
   }, 
   { 
     name: 'Program or be Programmed', 
     price: 8, 
     pubdate: new Date('2013', '08', '01'), 
     cover: 'img/space.png',
-    likes = 0
+    likes : 0
   } 
 ];
   $scope.plusOne = function(index) {
