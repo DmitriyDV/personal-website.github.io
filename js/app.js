b457a97f41865cbfe44f709f477a3118ed4080ba
DmitriyDV <31035451+DmitriyDV@users.noreply.github.com>
2017-09-19 14:50:24 +0300

    Update app.js
    add first settings for google map

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module("myFirstApp", []);
+//insert Google map
+  var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
