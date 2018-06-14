cd7dd4960773af39ade6f83a0472600c99f40af9
DmitriyDV <drozhzhin@telemed.help>
2018-06-14 21:51:32 +0300

    'first try, or not first'

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez',
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
+
+//ReactDOM.render(element, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
+
+
 //it is for google map(working)
 
 var map;
