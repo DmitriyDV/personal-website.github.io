f2fb720e5b79dc5d591f5abc02fe6e9333d6b086
DmitriyDV <drozhzhin@telemed.help>
2018-08-22 15:42:09 +0300

    "add LIKE one"

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 function formatName(user) {
   return user.firstName + ' ' + user.lastName;
 }
@@ -41,3 +42,30 @@ function initMap() {
     zoom: 8
   });
 }
+*/
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
